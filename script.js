9f64e3fe8081dd4b4395aeb3da3f48d4bc4d6f23
maryia.nekrasova <manekrasova85@gmail.com>
2021-10-06 10:22:20 +0300

    task5

--- script.js
+++ script.js
@@ -1,131 +1,111 @@
 "use strict";
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-const cartButton = document.querySelector('.cart-button');
-const searchButton = document.querySelector('.search-button');
-let searchInput = document.querySelector('.goods-search');
-searchButton.addEventListener('click', (e) => {
-  const value = searchInput.value;
-  list.filterGoods(value);
-});
 
-class GoodsItem {
-  constructor(product_name, price, img) {
-    this.product_name = product_name;
-    this.price = price;
-    this.img = img;
-  }
-  render() {
-    return `<div class="goods-item"><img alt="photo" src="images/${this.img}"><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
-  }
-}
-class GoodsList {
-  constructor() {
-    this.goods = [];
-    this.filteredGoods = [];
-  }
-  // fetchGoods() {
-  //   this.goods = [
-  //     { product_name: 'Suit', price: 150, img: 'product2.png'},
-  //     { product_name: 'Hoodie', price: 50, img: 'product3.png'},
-  //     { product_name: 'Jacket', price: 350, img: 'product5.png'},
-  //     { product_name: 'Trousers', price: 250, img: 'product4.png'},
-  //   ];
-  // }
-  async fetchGoods() {
-    return await fetch(`${API_URL}/catalogData.json`).then(resp => resp.json());
-  }
-  render() {
-    let listHtml = '';
-      this.goods.forEach(good => {
-        const goodItem = new GoodsItem(good.product_name, good.price, good.img);
-        listHtml += goodItem.render();
+const app = new Vue({
+  el: '#app',
+  data: {
+    goods: [],
+    filteredGoods: [],
+    basketGoods: [],
+    searchLine: '',
+    isVisibleCart: 'none',
+    allSumm: 0
+  },
+
+  methods: {
+    makeGETRequest(url)  {
+      return new Promise((resolve, reject) => {
+        let xhr;
+        if (window.XMLHttpRequest) {
+          xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+          xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+            resolve(JSON.parse(xhr.response));
+          }
+        }
+        xhr.onerror = function (error) {
+          reject(error);
+        }
+          xhr.open('GET', url, true);
+          xhr.send();
       });
-    document.querySelector('.goods-list').innerHTML = listHtml;
-  }
-  renderFilter () {
-    let listHtml = '';
-    this.filteredGoods.forEach(good => {
-      const goodItem = new GoodsItem(good.product_name, good.price, good.img);
-      listHtml += goodItem.render();
-    });
-    document.querySelector('.goods-list').innerHTML = listHtml;
-  }
-  calculateSumm(){
-    let summ = 0;
-    this.goods.forEach(good => {
-      summ += good.price;
-    });
-    console.log(summ);
-  }
-  filterGoods(value) {
-    const regexp = new RegExp(value, 'i');
-    this.filteredGoods = this.goods.filter(good => regexp.test(good.product_name));
-    this.renderFilter();
-  }
-}
+    },
 
-const makeGETRequest = (url) => {
- return new Promise((resolve, reject) => {
-  let xhr;
-  if (window.XMLHttpRequest) {
-    xhr = new XMLHttpRequest();
-  } else if (window.ActiveXObject) {
-    xhr = new ActiveXObject("Microsoft.XMLHTTP");
-  }
+    calculateSumm(){
+      let summ = 0;
+      this.basketGoods.forEach(good => {
+        summ += good.price*good.count;
+        this.allSumm = summ;
+      });
+    },
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      resolve(JSON.parse(xhr.response));
+    filterGoods() {
+      let text = this.searchLine.toLowerCase().trim();
+      if (text === '') {
+        this.filteredGoods = this.goods;
+      } else {
+        this.filteredGoods = this.goods.filter((el) => {
+          return el.product_name.toLowerCase().includes(text);
+        });
+      }
+    },
+
+    addProduct (good) {
+      let productId = good.id_product;
+      let count = 0;
+      if (this.basketGoods.length !== 0) {
+        this.basketGoods.forEach(arr => {
+          if(arr.id_product === productId){
+            arr.count = arr.count + 1;
+            arr.summBasket = arr.price * arr.count;
+          } else {
+            good.count = count + 1;
+            good.summBasket = good.price * good.count;
+            this.basketGoods.push(good);
+          }
+        });
+      }else {
+        good.count = count + 1;
+        good.summBasket = good.price * good.count;
+        console.log("add");
+        this.basketGoods.push(good);
+      }
+      this.calculateSumm();
+    },
+
+    openBasket() {
+      if (this.isVisibleCart === "none"){
+        this.isVisibleCart = "block";
+      } else {
+        this.isVisibleCart = "none";
+      }
+      this.calculateSumm();
     }
+
+  },
+
+  mounted() {
+    const promise = this.makeGETRequest(`${API_URL}/catalogData.json`);
+    promise
+    .then(
+      goods => {
+        this.goods = goods;
+        this.filteredGoods = goods;
+    }).catch(error => {
+     console.error(error.response);
+    });
   }
-
-  xhr.onerror = function (error) {
-    reject(error);
-  }
-
-  xhr.open('GET', url, true);
-  xhr.send();
- });
-}
-
-// class BasketItem {
-//   constructor() {
-//
-//   }
-//   render() {
-//
-//   }
-// }
-// class Basket {
-//   constructor() {
-//
-//   }
-//   render (){
-//
-//   }
-//   add (){
-//
-// }
-//   delete (){
-//
-// }
-//   calculateSumm(){
-//
-//   }
-// }
-const list = new GoodsList();
-const promise = list.fetchGoods();
-promise.then(goods => {
-  list.goods = goods;
-  list.render();
-  list.calculateSumm();
-}).catch(error => {
-  console.error(error.response);
 });
 
-//Решение Задания 4
-let regexp = /^'|(\s)'|'(\s)|'$/g;
-let str = "Mr 'Blue has' a blue aren't house and a blue car";
- let strAfter = str.replace(regexp, '$1"$2');
- console.log (strAfter);
-//
+
+
+
+
+
+
+
+
